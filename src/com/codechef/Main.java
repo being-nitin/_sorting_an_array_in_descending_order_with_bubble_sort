82ba4dfe862e9686ccf396eead9fe9fe0916e4e6
Nitin <nsinghboss02@gmail.com>
2021-12-26 11:03:49 +0530

    Initial commit

---
+++ src/com/codechef/Main.java
@@ -0,0 +1,22 @@
+package com.codechef;
+import java.util.Arrays;
+public class Main {
+
+    public static void main(String[] args) {
+	//
+      int[] num = {7,5,8,9,1};
+      descsort(num);
+        System.out.println(Arrays.toString(num));
+    }
+    public static void descsort(int[] num){
+        for(int i=0;i<num.length;i++){
+            for(int j=1;j< num.length-1;j++){
+                if(num[j-1]<num[j]){
+                    int temp = num[j-1];
+                    num[j-1] = num[j];
+                    num[j] = temp;
+                }
+            }
+        }
+    }
+}
